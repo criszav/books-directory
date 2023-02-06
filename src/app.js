84953e1dd2ed858d2b1e2179be94608ed99ca4d3
Cristian Zavala <czavala.dev@gmail.com>
2023-02-06 16:06:21 -0300

    add unrecognised url and error handler

--- src/app.js
+++ src/app.js
@@ -7,12 +7,13 @@ const app = express();
 const mongoose = require('mongoose');
 
 const bookRoutes = require('./routes/bookRoutes');
+const ExpressError = require('./utils/ExpressError');
 
 // Creating and connecting to DB
 mongoose.connect('mongodb://localhost:27017/books-directory');
 
 const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error: ")); 
+db.on("error", console.error.bind(console, "connection error: "));
 db.once("open", () => {
     console.log("Database connected");
 })
@@ -22,6 +23,20 @@ db.once("open", () => {
 app.use(express.json());
 // Routes
 app.use('/books', bookRoutes);
+
+
+// Unrecognise URL handler
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page not found', 404));
+})
+
+
+// Error handler
+app.use((err, req, res, next) => {
+    const { status = 500, message } = err;
+    if (!message) message = 'Something went wrong';
+    res.status(status).send(message);
+})
 
 
 
